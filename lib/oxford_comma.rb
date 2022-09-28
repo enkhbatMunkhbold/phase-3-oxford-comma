abef54f91c8c934ef81bca1b74e377146372b56c
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-09-27 17:47:57 -0700

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  str = ''
+  if array.length == 1
+    str = array.join
+  elsif array.length == 2
+    str = array.join(' and ')
+  else 
+    last = array.pop
+    str = array.join(', ')
+    str << ', and '
+    str << last 
+  end
+  str
 end
